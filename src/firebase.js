a31dacda82509d3ff67722a7543a0796349bb464
Anmol Kumar Srivastava <anmolsrivastava25@gmail.com>
2025-11-06 18:45:39 +0530

    Test

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDmjbxjeUi7Z-TIYFXFPBEcXvQRblOe_fw",
+  authDomain: "romance-retreat-xsavlab.firebaseapp.com",
+  projectId: "romance-retreat-xsavlab",
+  storageBucket: "romance-retreat-xsavlab.firebasestorage.app",
+  messagingSenderId: "1082057359683",
+  appId: "1:1082057359683:web:5cfcd253b2fc61a7712ff6"
 };
 
 const app = initializeApp(firebaseConfig);
